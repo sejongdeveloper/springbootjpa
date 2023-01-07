018b7c36db7e4699cf3226f08b9238b7bc962c9c
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-07 21:00:23 +0900

    스프링 데이터 6부: 스프링 데이터 JPA연동

--- src/test/java/me/whiteship/springbootjpa/account/AccountRepositoryTest.java
+++ src/test/java/me/whiteship/springbootjpa/account/AccountRepositoryTest.java
@@ -8,9 +8,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -26,11 +27,17 @@ public class AccountRepositoryTest {
 
     @Test
     public void di() throws SQLException {
-        try (Connection connection = dataSource.getConnection()){
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getURL());
-            System.out.println(metaData.getDriverName());
-            System.out.println(metaData.getUserName());
-        }
+        Account account = new Account();
+        account.setUsername("keesun");
+        account.setPassword("pass");
+
+        Account newAccount = accountRepository.save(account);
+        assertThat(newAccount).isNotNull();
+
+        Optional<Account> existingAccount = accountRepository.findByUsername(newAccount.getUsername());
+        assertThat(existingAccount).isNotEmpty();
+
+        Optional<Account> nonExistingAccount = accountRepository.findByUsername("whiteship");
+        assertThat(nonExistingAccount).isEmpty();
     }
 }
